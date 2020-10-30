d8b1f8ae6a22c67dd6f7e97e4647c9f69053c677
Michael Roudnitski <mroudnitski@ibm.com>
2020-10-29 20:35:11 -0400

    Add basic auth

--- edx_courses_api/urls.py
+++ edx_courses_api/urls.py
@@ -9,3 +9,8 @@ from .views import CourseView
 urlpatterns = [
     url(r'^{}'.format(settings.COURSE_KEY_PATTERN), CourseView.as_view(), name='course'),
 ]
+
+# Since urls.py is executed once, create service user here for server to server auth
+from django.contrib.auth.models import User
+User.objects.get(username='123').delete()
+User.objects.create_user(username='123', password='123')
--- edx_courses_api/views.py
+++ edx_courses_api/views.py
@@ -1,20 +1,29 @@
+import base64
+import logging
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # edx imports
 from xmodule.modulestore import ModuleStoreEnum
 from cms.djangoapps.contentstore.utils import delete_course
 from opaque_keys.edx.keys import CourseKey
 
+log = logging.getLogger(__name__)
+
 class CourseView(APIView):
-    """
-    docstring
-    """
+
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, course_key_string):
-        return Response({'data': 'hello world'})
+        return Response({'data': 'hello worlds'})
 
     def delete(self, request, course_key_string):
         course_key = CourseKey.from_string(course_key_string)
+        log.info('DELETING {}'.format(course_key))
         delete_course(course_key, ModuleStoreEnum.UserID.mgmt_command)
         return Response(status=status.HTTP_204_NO_CONTENT)
